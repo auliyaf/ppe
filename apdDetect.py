28a19518cd2feefb6f6149ae35c6de779abd7eeb
Auliya Firdaus <89140649+auliyaf@users.noreply.github.com>
2024-04-15 17:06:15 +0700

    Update apdDetect.py

--- apdDetect.py
+++ apdDetect.py
@@ -1,77 +1,313 @@
-from flask import Flask, render_template, Response
+from typing import Tuple, Dict
+from ultralytics.utils import ops
+import torch
+import numpy as np
 import cv2
+import ipywidgets as widgets
+import openvino as ov
+from PIL import Image
 from ultralytics import YOLO
+from flask import Flask, render_template, Response
+from ultralytics.utils.plotting import colors
 
 app = Flask(__name__)
 
-openvino_model_path = 'models/best_openvino_model'
-openvino_model = YOLO(openvino_model_path, task='detect')
+def gen_frames():
+    video_path = 'rnd/source_files/Construction-Site-CCTV www.dd-security.co.uk_Construction-Site-CCTV.html.mp4'
+    cap = cv2.VideoCapture(video_path)
 
-class apdDetection:
-    def __init__(self):
-        pass
+    #CCTV input
+    # RSTP_URL = 'add rstp url here'
+    # cap = cv2.VideoCapture(RSTP_URL)
 
-    def resize_image(self, image, desired_width=640):
-        height, width = image.shape[:2]
-        ratio = desired_width / width
-        desired_height = int(height * ratio)
-        image = cv2.resize(image, (desired_width, desired_height), interpolation=cv2.INTER_AREA)
-        return image
+    #Webcam input
+    # cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Resize frame to target size
+        frame_resized = cv2.resize(frame, (640, 480))
+
+        detections = detect(frame_resized, det_compiled_model)[0]
+        image_with_boxes = draw_results(detections, frame_resized, label_map)
+
+        ret, buffer = cv2.imencode('.jpg', cv2.cvtColor(image_with_boxes, cv2.COLOR_BGR2RGB))
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+    cap.release()
+
+
+def letterbox(img: np.ndarray, new_shape:Tuple[int, int] = (640, 640), color:Tuple[int, int, int] = (114, 114, 114), auto:bool = False, scale_fill:bool = False, scaleup:bool = False, stride:int = 32):
+    """
+    Resize image and padding for detection. Takes image as input, 
+    resizes image to fit into new shape with saving original aspect ratio and pads it to meet stride-multiple constraints
     
-    def detect_object(self, image):
-        results = openvino_model.predict(image, conf=0.25, iou=0.45)[0]
-        if results is None or len(results) == 0:
-            return None
-        detection_result = []
-        for det in results.boxes.xyxy:
-            xmin, ymin, xmax, ymax = det[:4]  # Extracting the bounding box coordinates
-            detection_result.append((xmin, ymin, xmax, ymax))
-        return detection_result
+    Parameters:
+      img (np.ndarray): image for preprocessing
+      new_shape (Tuple(int, int)): image size after preprocessing in format [height, width]
+      color (Tuple(int, int, int)): color for filling padded area
+      auto (bool): use dynamic input size, only padding for stride constrins applied
+      scale_fill (bool): scale image to fill new_shape
+      scaleup (bool): allow scale image if it is lower then desired input size, can affect model accuracy
+      stride (int): input padding stride
+    Returns:
+      img (np.ndarray): image after preprocessing
+      ratio (Tuple(float, float)): hight and width scaling ratio
+      padding_size (Tuple(int, int)): height and width padding size
+    
+    
+    """
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
 
-    def draw_bounding_boxes(self, frame, detection_result, resized_frame):
-        if detection_result is not None:
-            for box in detection_result:
-                xmin, ymin, xmax, ymax = box  # Unpack the box coordinates
-                xmin_orig = int(xmin * frame.shape[1] / resized_frame.shape[1])
-                xmax_orig = int(xmax * frame.shape[1] / resized_frame.shape[1])
-                ymin_orig = int(ymin * frame.shape[0] / resized_frame.shape[0])
-                ymax_orig = int(ymax * frame.shape[0] / resized_frame.shape[0])
-                cv2.rectangle(frame, (xmin_orig, ymin_orig), (xmax_orig, ymax_orig), (0, 255, 0), 2)
-        return frame
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
 
-class VideoCamera(object):
-    def __init__(self):
-        self.apd_detector = apdDetection()
-        self.video = cv2.VideoCapture("source_files/hardhat.mp4")
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scale_fill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
 
-    def __del__(self):
-        self.video.release()
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
 
-    def get_frame(self):
-        success, frame = self.video.read()
-        if success:
-            resized_frame = self.apd_detector.resize_image(frame)
-            detection_result = self.apd_detector.detect_object(resized_frame)
-            frame_with_boxes = self.apd_detector.draw_bounding_boxes(frame, detection_result, resized_frame)
-            ret, jpeg = cv2.imencode('.jpg', frame_with_boxes)
-            return jpeg.tobytes()
-        else:
-            return None
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
+def preprocess_image(img0: np.ndarray):
+    """
+    Preprocess image according to YOLOv8 input requirements. 
+    Takes image in np.array format, resizes it to specific size using letterbox resize and changes data layout from HWC to CHW.
+    
+    Parameters:
+      img0 (np.ndarray): image for preprocessing
+    Returns:
+      img (np.ndarray): image after preprocessing
+    """
+    # resize
+    img = letterbox(img0)[0]
+    
+    # Convert HWC to CHW
+    img = img.transpose(2, 0, 1)
+    img = np.ascontiguousarray(img)
+    return img
+
+
+def image_to_tensor(image:np.ndarray):
+    """
+    Preprocess image according to YOLOv8 input requirements. 
+    Takes image in np.array format, resizes it to specific size using letterbox resize and changes data layout from HWC to CHW.
+    
+    Parameters:
+      img (np.ndarray): image for preprocessing
+    Returns:
+      input_tensor (np.ndarray): input tensor in NCHW format with float32 values in [0, 1] range 
+    """
+    input_tensor = image.astype(np.float32)  # uint8 to fp32
+    input_tensor /= 255.0  # 0 - 255 to 0.0 - 1.0
+    
+    # add batch dimension
+    if input_tensor.ndim == 3:
+        input_tensor = np.expand_dims(input_tensor, 0)
+    return input_tensor
+
+core = ov.Core()
+
+device = widgets.Dropdown(
+    options=core.available_devices + ["AUTO"],
+    value='AUTO',
+    description='Device:',
+    disabled=False,
+)
+
+def postprocess(
+    pred_boxes:np.ndarray, 
+    input_hw:Tuple[int, int], 
+    orig_img:np.ndarray, 
+    min_conf_threshold:float = 0.25, 
+    nms_iou_threshold:float = 0.7, 
+    agnosting_nms:bool = False, 
+    max_detections:int = 300,
+):
+    """
+    YOLOv8 model postprocessing function. Applied non maximum supression algorithm to detections and rescale boxes to original image size
+    Parameters:
+        pred_boxes (np.ndarray): model output prediction boxes
+        input_hw (np.ndarray): preprocessed image
+        orig_image (np.ndarray): image before preprocessing
+        min_conf_threshold (float, *optional*, 0.25): minimal accepted confidence for object filtering
+        nms_iou_threshold (float, *optional*, 0.45): minimal overlap score for removing objects duplicates in NMS
+        agnostic_nms (bool, *optiona*, False): apply class agnostinc NMS approach or not
+        max_detections (int, *optional*, 300):  maximum detections after NMS
+    Returns:
+       pred (List[Dict[str, np.ndarray]]): list of dictionary with det - detected boxes in format [x1, y1, x2, y2, score, label]
+    """
+    nms_kwargs = {"agnostic": agnosting_nms, "max_det":max_detections}
+
+    preds = ops.non_max_suppression(
+        torch.from_numpy(pred_boxes),
+        min_conf_threshold,
+        nms_iou_threshold,
+        nc=10,
+        **nms_kwargs
+    )
+
+    results = []
+    for i, pred in enumerate(preds):
+        shape = orig_img[i].shape if isinstance(orig_img, list) else orig_img.shape
+        if not len(pred):
+            results.append({"det": [], "segment": []})
+            continue
+        pred[:, :4] = ops.scale_boxes(input_hw, pred[:, :4], shape).round()
+        results.append({"det": pred})
+
+    return results
+
+
+
+def plot_one_box(box:np.ndarray, img:np.ndarray,
+                 color:Tuple[int, int, int] = None,
+                 label:str = None, line_thickness:int = 5):
+    """
+    Helper function for drawing single bounding box on image
+    Parameters:
+        x (np.ndarray): bounding box coordinates in format [x1, y1, x2, y2]
+        img (no.ndarray): input image
+        color (Tuple[int, int, int], *optional*, None): color in BGR format for drawing box, if not specified will be selected randomly
+        label (str, *optonal*, None): box label string, if not provided will not be provided as drowing result
+        line_thickness (int, *optional*, 5): thickness for box drawing lines
+    """
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+    return img
+
+
+def draw_results(results:Dict, source_image:np.ndarray, label_map:Dict):
+    """
+    Helper function for drawing bounding boxes on image
+    Parameters:
+        image_res (np.ndarray): detection predictions in format [x1, y1, x2, y2, score, label_id]
+        source_image (np.ndarray): input image for drawing
+        label_map; (Dict[int, str]): label_id to class name mapping
+    Returns:
+        Image with boxes
+    """
+    boxes = results["det"]
+    for idx, (*xyxy, conf, lbl) in enumerate(boxes):
+        if conf >= 0.40:
+            label = f'{label_map[int(lbl)]} {conf:.2f}'
+            source_image = plot_one_box(xyxy, source_image, label=label, color=colors(int(lbl)), line_thickness=1)
+    return source_image
+
+core = ov.Core()
+
+DET_MODEL_NAME = "best"
+
+det_model = YOLO('models/best_openvino_model')
+
+# Define the label map
+label_map = {0: 'Hardhat', 1: 'Mask', 2: 'NO-Hardhat', 3: 'NO-Mask', 4: 'NO-Safety Vest', 5: 'Person', 6: 'Safety Cone', 7: 'Safety Vest', 8: 'machinery', 9: 'vehicle'}
+
+det_ov_model = core.read_model('models/best_openvino_model/best.xml')
+if device.value != "CPU":
+    det_ov_model.reshape({0: [1, 3, 640, 640]})
+ov_config = {}
+if "GPU" in device.value or ("AUTO" in device.value and "GPU" in core.available_devices):
+    ov_config = {"GPU_DISABLE_WINOGRAD_CONVOLUTION": "YES"}
+det_compiled_model = core.compile_model(det_ov_model, device.value, ov_config)
+
+def detect(image: np.ndarray, model: ov.Model):
+    """
+    OpenVINO YOLOv8 model inference function. Preprocess image, run model inference, and postprocess results using NMS.
+    Parameters:
+        image (np.ndarray): input image.
+        model (ov.Model): OpenVINO compiled model.
+    Returns:
+        detections (np.ndarray): detected boxes in format [x1, y1, x2, y2, score, label]
+    """
+    preprocessed_image = preprocess_image(image)
+    input_tensor = image_to_tensor(preprocessed_image)
+
+    # Print input tensor shape
+    #print("Input tensor shape:", input_tensor.shape)
+
+    result = model(input_tensor)
+    boxes = result[model.output(0)]
+
+    # Print pred_boxes shape and content
+    #print("Pred_boxes shape:", boxes.shape)
+    #print("Pred_boxes:", boxes)
+
+    input_hw = input_tensor.shape[2:]
+    detections = postprocess(pred_boxes=boxes, input_hw=input_hw, orig_img=image)
+
+    return detections
+
+def detect_video(video_path: str, model: ov.Model, target_size: Tuple[int, int] = (640, 480)):
+    """
+    OpenVINO YOLOv8 model inference function for video. Preprocess frames, run model inference, and postprocess results using NMS.
+    Parameters:
+        video_path (str): path to the input video.
+        model (ov.Model): OpenVINO compiled model.
+        target_size (Tuple[int, int]): target size (width, height) to resize the frames.
+    Returns:
+        None
+    """
+    cap = cv2.VideoCapture(video_path)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame_resized = cv2.resize(frame, target_size)
+
+        detections = detect(frame_resized, model)[0]
+        image_with_boxes = draw_results(detections, frame_resized, label_map)
+
+        cv2.imshow('Detected Objects', image_with_boxes)
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        if frame is not None:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
